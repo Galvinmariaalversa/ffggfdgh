b6c52a4e18ebf4ade9cdf99273a2dadba29f989d
Galvin J <galvin.ma.j@gmail.com>
2025-02-27 11:09:48 +0530

    first commit

---
+++ fs.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector('.menu-icon');
+        const navMenu = document.querySelector('nav ul');
+
+        menuIcon.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+        });
